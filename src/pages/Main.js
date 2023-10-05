a9f4f942a09a858e4584b771c4fbb53bb4cff3c8
mikebds <rchallapa@bancodelsol.com>
2023-10-05 01:03:55 -0300

    update background image

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -4,7 +4,24 @@ import { Counter } from '../components/Counter';
 export const Main = () => {
   return (
     <div className="h-screen flex lg:px-0 px-5">
-      <div className="lg:w-3/5 lg:self-center mt-16 lg:mt-0">
+      <div className="hidden lg:block w-2/5 lg:w-3/5 sm:w-4/5 bg-black">
+        <picture className="">
+          <img src="./images/fondo1.png" className="w-full h-full object-cover" alt="profile" />
+        </picture>
+      </div>
+      <div className="absolute bottom-0 lg:-bottom-32 left-0 right-0">
+        <img src="./images/ondas.svg" className="" alt="onda" />
+      </div>
+
+      <div className="absolute bottom-0 left-1/2 w-60 h-60 lg:w-72 lg:h-72 z-10 -translate-x-1/2 bg-[url('../public/images/circuloContador.svg')] bg-no-repeat bg-contain">
+        <div className="w-full h-full flex flex-col items-center justify-center">
+          <p className="text-center text-[#BFA880] text-1xl">Faltan:</p>
+          <Counter />
+          <FaHeart className="animate-bounce" color="#81938A" size={35} />
+        </div>
+      </div>
+
+      <div className="lg:w-3/5 w-full lg:self-center mt-16 lg:mt-0">
         <div className="flex justify-center items-center mb-5">
           <div className="w-10 lg:w-20 h-0.5 bg-[#BFA880]"></div>
           <p className="mx-5 text-2xl text-[#81938A]">28.10.2023</p>
@@ -24,23 +41,6 @@ export const Main = () => {
             Vení, te esperamos para pasar un momento inolvidable!!
           </p>
           <FaQuoteRight color='#BFA880' />
-        </div>
-      </div>
-
-      <div className="hidden lg:block w-2/5 lg:w-3/5 sm:w-4/5 bg-black">
-        <picture className="">
-          <img src="./images/fondo.png" className="w-full h-full object-cover" alt="profile" />
-        </picture>
-      </div>
-      <div className="absolute bottom-0 lg:-bottom-32 left-0 right-0">
-        <img src="./images/ondas.svg" className="" alt="onda" />
-      </div>
-
-      <div className="absolute bottom-0 left-1/2 w-60 h-60 lg:w-72 lg:h-72 z-10 -translate-x-1/2 bg-[url('../public/images/circuloContador.svg')] bg-no-repeat bg-contain">
-        <div className="w-full h-full flex flex-col items-center justify-center">
-          <p className="text-center text-[#BFA880] text-1xl">Faltan:</p>
-          <Counter />
-          <FaHeart className="animate-bounce" color="#81938A" size={35} />
         </div>
       </div>
     </div>
